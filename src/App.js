ab384dbb5a58dad1ae589c70b3419cbe0f477ccd
balagowda <balagowda9483@gmail.com>
2023-09-14 23:08:28 +0530

    Page design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import Speech from "./Speech";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Application
-      </header>
+      <h1 className="header">Speech Converter</h1>
+      <div className="container">
+        <Speech />
+      </div>
     </div>
   );
 }
---
+++ src/Speech.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Speech = () => {
+  return (
+    <div className="container-box">
+        <div className="bottom-div">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Speech
