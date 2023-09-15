9db5f70d39e493e1596f640f77b226498f366673
balagowda <balagowda9483@gmail.com>
2023-09-15 19:58:37 +0530

    speech recognition updates and copy click board

--- src/Speech.jsx
+++ src/Speech.jsx
@@ -1,14 +1,75 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MicIcon from "@mui/icons-material/Mic";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import Tooltip from "@mui/material/Tooltip";
 
 const Speech = () => {
+  const [speak, setSpeak] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const stopListening = () => SpeechRecognition.stopListening();
+
+  const [isCopied, setCopied] = useClipboard(transcript);
+
+  const handleClick = () => {
+    setSpeak(!speak);
+    if (speak) {
+      stopListening();
+    } else {
+      startListening();
+    }
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+
+    // Automatically close the tooltip after 2 seconds
+    setTimeout(() => {
+      setOpen(false);
+    }, 1500);
+  };
+
+  const handleClickCopy =()=>{
+      setCopied();
+      handleOpen();
+  } 
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
     <div className="container-box">
-        <div className="bottom-div">
-            
-        </div>
-    </div>
-  )
-}
+      <div className="content">{transcript}</div>
 
-export default Speech
+      <div className="bottom-div">
+        {transcript && !speak && (
+          <div className="bottom-child btn1">
+            <Tooltip title="copied" arrow placement="top" open={open} onClose={() => setOpen(false)}>
+              <button className="btn-child" onClick={handleClickCopy}>
+                Copy
+              </button>
+            </Tooltip>
+          </div>
+        )}
+
+        <div className="bottom-child btn-2">
+          <div className={speak ? "behind animate" : "behind"}></div>
+          <div className="front" onClick={handleClick}>
+            <MicIcon className="mic" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Speech;
